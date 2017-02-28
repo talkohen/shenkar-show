ee8c9459d8ed415f910c3f08c0757ecbb7bfe846
Tal <tal@vees.to>
2017-02-28 10:21:33 +0200

    project api

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -44,3 +44,114 @@ exports.getProjectByName  = function (req, res) {
         return;
     });
 }
+
+
+exports.createProject = function (request, response) {
+
+    project.find({name : request.body.name },function(err, doc){
+       if (doc.length){
+            console.log("project already exists");
+            response.send ("project already exists");
+          }else{
+          	
+          
+          	
+             var newProject = new project({
+                name :request.body.name,
+                department :request.body.department,
+                students :request.body.students,
+                description :request.body.description,
+                image :request.body.image,
+                video :request.body.video,
+                audio :request.body.audio,
+                location :request.body.location,
+				likes :request.body.likes,
+                comments :request.body.comments,
+                QRcode :request.body.QRcode
+              });
+              
+           try {
+              newProject.save(function(error, result) {
+                if (error) {
+                  console.error(error);
+                } else {
+                  console.log("Saved document : " + doc);
+                  response.send (true);
+                };
+              });
+              }
+               catch (exception) {
+ 	console.log (exception); 
+ 	response.send (false);
+ }
+            }
+          });    
+
+
+
+
+}
+
+exports.updateProject = function (request, response) {
+
+	 var query = project.findOne().where ('name', request.body.name);
+	 
+	 query.exec (function (err,doc) {
+	
+	try {	
+	 	var query = doc.update ({
+	 		$set: {
+	 				name :request.body.name,
+	                department :request.body.department,
+	                students :request.body.students,
+	                description :request.body.description,
+	                image :request.body.image,
+	                video :request.body.video,
+	                audio :request.body.audio,
+	                location :request.body.location,
+					likes :request.body.likes,
+	                comments :request.body.comments,
+	                QRcode :request.body.QRcode
+	 		}	 		
+	 	});
+ 	
+ 	 	query.exec (function (err, results) {
+ 		console.log ("\n Resulets Object : " + JSON.stringify (results));
+ 	});
+ 	      	
+			console.log("Updated Doc : " + doc);
+            response.send (true);
+            
+ 	}
+ catch (exception) {
+ 	console.log (exception); 
+ 	response.send (false);
+ }
+
+ });
+}
+
+
+exports.deleteProject = function (request, response) {
+
+	var query = project.findOne().where ('name', request.body.name);
+	
+	 query.exec (function (err,doc) {
+	 		try {	
+	 	var query = doc.remove (function (err, deletedDoc) {
+	 		project.findOne ({name: request.body.name}, function (err, doc) {
+	 			console.log("Removed doc : " + doc);
+                  response.send (true);
+	 		})
+	 	});
+	 	
+	 	}
+	 	catch (exception) {
+ 	console.log (exception); 
+ 	response.send (false);
+ }
+	 	
+	 });
+
+}
+
