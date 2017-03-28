1453765a40f808928d0b71accc836774f3396c2a
Tal <tal@vees.to>
2017-03-28 08:14:45 +0300

    crash fix

---
+++ credentials.js
@@ -0,0 +1,5 @@
+var accessKeyId = "AKIAJZRJ5WQJ5LETQCIA";
+var secretAccessKey = "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";
+
+exports.accessKeyId = accessKeyId;
+exports.secretAccessKey = secretAccessKey;
