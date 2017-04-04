6c6078fc1ca6583b6fc37b0f6b09187e3c1e40f4
Tal <tal@vees.to>
2017-04-04 12:12:02 +0300

    allow credentials

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ app.use ('/' , express.static ('./public'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   app.set ('json spaces', 4);
   res.set ("Content-Type", "application/json");
