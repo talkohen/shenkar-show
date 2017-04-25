d814d1055c388043e9b1b5772733dec3ad76707b
talkohen <coen.tal@gmail.com>
2017-04-25 22:19:14 +0300

    routes

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -262,7 +262,7 @@ exports.getDepartmentProjects = function (req, res) {
 	var id = req.params.departmentId;
 	
 	
-	project.find ({departmentId : id}).exec (function (err, projects) {
+	project.find ({departmentId : id}).populate('location').exec (function (err, projects) {
 		
 			res.send (projects);
 		
