493ca2ae2fef1161e264c5f1eefeac567498751b
Tal <tal@vees.to>
2017-05-09 15:46:44 +0300

    test

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.use ('/' , express.static ('./public'));
 
 app.use(function(req, res, next) {
 
-  res.header("Access-Control-Allow-Origin", 'https://shenkar-show-web-new.herokuapp.com/');
+  res.header("Access-Control-Allow-Origin", 'https://shenkar-show-web-new.herokuapp.com');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
