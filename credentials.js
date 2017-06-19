32d3eff9dda4a569fff9c5623461ada04c94b4ac
Tal Kohen <coen.tal@gmail.com>
2017-06-19 12:42:38 +0300

    Delete credentials.js

--- credentials.js
+++
@@ -1,5 +0,0 @@
-var accessKeyId = "AKIAIE7YYI3U2ZQLOHFA";
-var secretAccessKey = "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";
-
-exports.accessKeyId = accessKeyId;
-exports.secretAccessKey = secretAccessKey;
